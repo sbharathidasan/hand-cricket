2f46b5290bee7bd56805eb85f87723da0c53ec75
Bharathi <bharathidasan2072005@gmail.com>
2024-06-01 17:02:04 +0530

    added search button functionalities

--- keep.js
+++ keep.js
@@ -1,10 +1,32 @@
 var notes=document.getElementById("writeNotes");
 var container=document.getElementById("container");
+var searcharr=["WRITE YOUR NOTES"];
+var searchElement=document.getElementById("search");
 function create(){
-  var divElement=document.createElement('div');
-  divElement.innerHTML="<p>"+notes.value+"<button onclick='del(event)'>delete</button></p>";
-  container.append(divElement);
-  notes.value="";
+  if(notes.value!==''){
+      var divElement=document.createElement('div');
+      divElement.innerHTML="<p>"+notes.value+"<button onclick='del(event)'>delete</button></p>";
+      var temp=(notes.value).toUpperCase();
+      searcharr.push(temp);
+      container.append(divElement);
+      notes.value="";
+    }
+    else{
+      alert("notes shouldnotbe empty")
+    }
+}
+function search(){
+  let j=0;
+  for(var i=0;i<searcharr.length;i++){
+    if ((searchElement.value).toUpperCase()===searcharr[i]){
+      alert("available!");
+      j=j+1;
+      break;
+    }
+  }
+  if (j===0){
+    alert("not available!")
+  }
 }
 function del(eve){
   eve.target.parentNode.parentNode.remove();
